4aa8af4ce134456bb35a02382255e5c38866eeb4
Helena Oliveira <helena@informach.com.br>
2022-05-14 16:23:42 -0300

    add inputs, services, models and resolvers

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -4,7 +4,12 @@ import { DatabaseModule } from 'src/database/database.module';
 import path from 'node:path';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
-import { TestResolver } from './test.resolver';
+import { ProductsResolver } from './graphql/resolvers/products.resolver';
+import { ProductsService } from 'src/services/products.service';
+import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
+import { PurchasesService } from 'src/services/purchases.service';
+import { CustomersResolver } from './graphql/resolvers/customers.resolver';
+import { CustomersService } from 'src/services/customers.service';
 
 @Module({
   imports: [
@@ -15,6 +20,13 @@ import { TestResolver } from './test.resolver';
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  providers: [TestResolver],
+  providers: [
+    ProductsResolver,
+    ProductsService,
+    PurchasesResolver,
+    PurchasesService,
+    CustomersResolver,
+    CustomersService,
+  ],
 })
 export class HttpModule {}
